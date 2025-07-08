aa3147ab0f88f36306313fce903575baabbbdfc6
shahzanms <99734718+shahzanms@users.noreply.github.com>
2025-07-08 23:10:33 +0530

    Update get-query-client.ts

--- app/_lib/get-query-client.ts
+++ app/_lib/get-query-client.ts
@@ -2,22 +2,20 @@ import { isServer, QueryClient } from "@tanstack/react-query";
 
 let queryClientInstance: QueryClient | null = null;
 export const getQueryClient = () => {
- if(isServer){
-  return makeQueryCLient();
- }
- else{
-  if(!queryClientInstance)queryClientInstance = makeQueryCLient();
-  return queryClientInstance
- }
- 
-}
+  if (isServer) {
+    return makeQueryCLient();
+  } else {
+    if (!queryClientInstance) queryClientInstance = makeQueryCLient();
+    return queryClientInstance;
+  }
+};
 
-const makeQueryCLient=()=>{
+const makeQueryCLient = () => {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60*1000,
+        staleTime: 60 * 1000,
       },
     },
-  })
-}
+  });
+};
