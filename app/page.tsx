27ddb58e4e7f090f7ad06a5477adf246051e6e28
shahzanms <99734718+shahzanms@users.noreply.github.com>
2025-07-08 23:26:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
 import UsersList from "./_components/UsersList";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getUsers } from "./_lib/data-service";
-import { UsersResponse } from "./_models/user.model";
 import React from "react";
-import { getQueryClient } from "./_lib/get-query-client";
 
 interface HomePageProps {
   dehydratedState: any;
 }
 const HomePage: React.FC<HomePageProps> = async () => {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery<UsersResponse, Error>({
-    queryKey: ["users", 1],
-    queryFn: () => getUsers(1),
-  });
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
       <UsersList />
-    </HydrationBoundary>
   );
 };
-
 export default HomePage;
