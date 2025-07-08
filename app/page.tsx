df104115e5b7c3d552636d9a3cdcd5b738842783
shahzanms <99734718+shahzanms@users.noreply.github.com>
2025-07-08 23:11:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import UsersList from "./_components/UsersList";
-import {
-  dehydrate,
-  QueryClient,
-  HydrationBoundary,
-} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getUsers } from "./_lib/data-service";
 import { UsersResponse } from "./_models/user.model";
 import React from "react";
-import Header from "./_components/Header";
-import { Box, Center, Text } from "@chakra-ui/react";
+import { getQueryClient } from "./_lib/get-query-client";
 
 interface HomePageProps {
   dehydratedState: any;
 }
 const HomePage: React.FC<HomePageProps> = async () => {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
   await queryClient.prefetchQuery<UsersResponse, Error>({
     queryKey: ["users", 1],
-    queryFn: () => getUsers(1)
+    queryFn: () => getUsers(1),
   });
 
   return (
